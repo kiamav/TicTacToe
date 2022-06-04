3421366b792f0f9a4eaf11f737688047810ecf38
Kiarash Mavandad <kmavandad@squareup.com>
2022-06-04 01:47:21 -0700

    rename private vars and move event callback to board class

--- board.py
+++ board.py
@@ -3,29 +3,28 @@ from board_square import BoardSquare
 
 class Board:
     def __init__(self, canvas, board_size, n):
-        self.canvas = canvas
-        self.board_size = board_size
-        self.n = n
-        self.square_len = board_size / self.n
-        self.x_turn = True
-        self._init_grid()
+        self._canvas = canvas
+        self._board_size = board_size
+        self._n = n
+        self._square_len = board_size / self._n
+        self._x_turn = True
+        self.grid = [[BoardSquare(self._square_len * i, self._square_len * j, self._canvas, self._square_len)
+                      for j in range(self._n)] for i in range(self._n)]
         self._draw_board()
+        self._canvas.bind('<Button-1>', self.move)
 
     def _draw_board(self):
-        for i in range(1, self.n):
-            self.canvas.create_line(self.square_len * i, 0, self.square_len * i, self.board_size, width=5)
-            self.canvas.create_line(0, self.square_len * i, self.board_size, self.square_len * i, width=5)
-        self.canvas.pack()
+        for i in range(1, self._n):
+            self._canvas.create_line(self._square_len * i, 0, self._square_len * i, self._board_size, width=5)
+            self._canvas.create_line(0, self._square_len * i, self._board_size, self._square_len * i, width=5)
+        self._canvas.pack()
 
-    def _init_grid(self):
-        self.grid = [[BoardSquare(self.square_len*i, self.square_len*j, self.canvas, self.square_len)
-                      for j in range(self.n)] for i in range(self.n)]
-
-    def move(self, x, y):
-        # place an X or O at x,y coordinate square based on whose turn it is
-        grid_i = int(x//self.square_len)
-        grid_j = int(y//self.square_len)
+    def move(self, event):
+        # we get a callback to this method when the left mouse is clicked. the event contains
+        # the x,y coord of where was clicked within the canvas
+        grid_i = int(event.x // self._square_len)
+        grid_j = int(event.y // self._square_len)
         board_square: BoardSquare = self.grid[grid_i][grid_j]
-        is_valid_move = board_square.move(self.x_turn)
+        is_valid_move = board_square.fill_square(self._x_turn)
         if is_valid_move:
-            self.x_turn = not self.x_turn
+            self._x_turn = not self._x_turn
--- starter.py
+++ starter.py
@@ -4,17 +4,10 @@ from board import Board
 BOARD_SIZE = 1000
 # number of squares in width/length. total num squares is NUM_SQUARES^2
 NUM_SQUARES = 3
-SQUARE_LEN = BOARD_SIZE / NUM_SQUARES
-OFFSET = SQUARE_LEN * 0.1
 
 root = Tk()
 canvas = Canvas(width=BOARD_SIZE, height=BOARD_SIZE)
 board = Board(canvas, BOARD_SIZE, NUM_SQUARES)
 
 
-def test_event(event):
-	board.move(event.x, event.y)
-
-
-canvas.bind('<Button-1>', test_event)
 root.mainloop()
