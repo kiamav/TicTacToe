5c8265907b15d893ebf1f122d5298ef451474408
Kiarash Mavandad <kmavandad@squareup.com>
2022-06-04 02:50:41 -0700

    rename

--- starter.py
+++ starter.py
@@ -2,10 +2,10 @@ from tkinter import Tk, Canvas
 from board import Board
 
 BOARD_SIZE = 1000
-# number of squares in width/length. total num squares is NUM_SQUARES^2
-NUM_SQUARES = 3
+# dimensions in width/length. total num squares is DIMENSIONS^2
+DIMENSIONS = 3
 
 root = Tk()
 canvas = Canvas(width=BOARD_SIZE, height=BOARD_SIZE)
-board = Board(canvas, BOARD_SIZE, NUM_SQUARES)
+board = Board(canvas, BOARD_SIZE, DIMENSIONS)
 root.mainloop()
